d71e38351191bc19ce4d7939d3faf92a2ff9ec8c
Alok Parlikar <alok@cobaltspeech.com>
2019-04-26 10:55:16 +0530

    add top-level Makefile
    Added a top-level Makefile to set up dependencies and generate code and docs.

---
+++ ci/validate_generated_artifacts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copyright (2019) Cobalt Speech and Language Inc.
+
+set -e
+
+# install system dependencies (ci assumes golang:1.12 as the base image)
+apt update && apt install -y python3 python-virtualenv unzip
+
+# generate all artifacts again
+make
+
+# it's an error if we generated something that wasn't checked in.
+git diff --quiet || exit 1
