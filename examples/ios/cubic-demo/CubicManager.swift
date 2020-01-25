22d84ae01c5b287d3352d56d0bc9b108ef77de4f
Alex Mankov <tumarsal@gmail.com>
2020-01-25 22:08:43 +0300

    add stream methods

--- examples/ios/cubic-demo/CubicManager.swift
+++ examples/ios/cubic-demo/CubicManager.swift
@@ -12,7 +12,8 @@ import sdk_cubic
 public protocol CubicManagerDelegate {
     
     func managerDidRecognizeWithResponse(_ res: Cobaltspeech_Cubic_RecognitionResponse)
-    
+    func streamCompletion(_ result:CallResult?)
+    func streamReceive(_ result:ResultOrRPCError<Cobaltspeech_Cubic_RecognitionResponse?>)
 }
 
 
@@ -22,6 +23,11 @@ public class CubicManager: NSObject, AVAudioRecorderDelegate {
     private let client: Cobaltspeech_Cubic_CubicServiceClient
     private var whistleRecorder: AVAudioRecorder!
     private var selectedModelId: String = "\(1)"
+    private var callStream:Cobaltspeech_Cubic_CubicStreamingRecognizeCall?
+    private var audioEngine : AVAudioEngine!
+    private var audioFile : AVAudioFile!
+    private var outref: ExtAudioFileRef?
+    private var filePath : String? = nil
     public var delegate: CubicManagerDelegate?
     var isRecord = false
     public var selectedModel: Cobaltspeech_Cubic_Model? {
@@ -76,7 +82,142 @@ public class CubicManager: NSObject, AVAudioRecorderDelegate {
     private class func getWavURL(_ name:String = "") -> URL {
         return CubicManager.getDocumentsDirectory().appendingPathComponent("whistle\(name).wav")
     }
-    
+    public func startStream() {
+         do {
+            let call = try self.client.streamingRecognize(completion: { (callResult) in
+                    self.delegate?.streamCompletion(callResult)
+            })
+            try call.receive { res in
+                DispatchQueue.main.async {
+                    self.delegate?.streamReceive(res)
+                }
+            }
+            var conReq = Cobaltspeech_Cubic_StreamingRecognizeRequest()
+            conReq.config.modelID = self.selectedModelId
+            conReq.config.idleTimeout.seconds = 5
+            conReq.config.audioEncoding = .rawLinear16
+            try call.send(conReq)
+            callStream = call
+            self.startAudioEngine()
+        } catch let e {
+             self.log("Error \(e)")
+        }
+    }
+    func startAudioEngine() {
+        
+        try! AVAudioSession.sharedInstance().setCategory(.playAndRecord)
+        try! AVAudioSession.sharedInstance().setActive(true)
+
+        if audioEngine == nil {
+            audioEngine = AVAudioEngine()
+        }
+
+        let format = AVAudioFormat(commonFormat: AVAudioCommonFormat.pcmFormatInt16,
+                                                     sampleRate: 44100.0,
+                                                     channels: 1,
+                                                     interleaved: true)
+        let settings:[String : Any] = [
+            AVFormatIDKey: Int(kAudioFormatLinearPCM),
+            AVSampleRateKey: 16000.0,
+            AVNumberOfChannelsKey: 1,
+            AVLinearPCMBitDepthKey: 16,
+            AVLinearPCMIsFloatKey:false,
+            AVLinearPCMIsBigEndianKey:false,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+        let downFormat = AVAudioFormat(settings: settings)
+        audioEngine.connect(audioEngine.inputNode, to: audioEngine.mainMixerNode, format: format)
+        /*If you want record to file */
+        let dir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first! as String
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMdd_HHmmss"
+        let filePath =  dir.appending(String(format: "/%@.wav", formatter.string(from: Date())))
+        let outurl = URL(fileURLWithPath: filePath)
+        self.filePath = filePath
+        _ = ExtAudioFileCreateWithURL(outurl as CFURL,
+            kAudioFileWAVEType,
+            downFormat!.streamDescription,
+            nil,
+            AudioFileFlags.eraseFile.rawValue,
+            &outref)
+        /*if you want record to file end */
+        audioEngine.inputNode.installTap(onBus: 0,
+                                         bufferSize: AVAudioFrameCount(format!.sampleRate * 0.4),
+                                         format: format,
+                                         block: { (buffer: AVAudioPCMBuffer!, time: AVAudioTime!) -> Void in
+
+            let converter = AVAudioConverter(from: format!, to: downFormat!)
+            let newbuffer = AVAudioPCMBuffer(pcmFormat: downFormat!,
+                frameCapacity: AVAudioFrameCount(downFormat!.sampleRate * 0.4))
+            let inputBlock : AVAudioConverterInputBlock = { (inNumPackets, outStatus) -> AVAudioBuffer? in
+                outStatus.pointee = AVAudioConverterInputStatus.haveData
+                let audioBuffer : AVAudioBuffer = buffer
+                return audioBuffer
+            }
+            if let nb = newbuffer {
+                var error : NSError?
+                //converter!.convert(to: nb, from: buffer)
+                converter!.convert(to: nb, error: &error, withInputFrom: inputBlock)
+                self.sendStreamPartToServer(nb,time)
+                _ = ExtAudioFileWrite(self.outref!, nb.frameLength, nb.audioBufferList)
+            }
+        })
+
+        try! audioEngine.start()
+        self.isRecord = true
+    }
+    private func sendStreamPartToServer(_ buffer:AVAudioPCMBuffer,_ time:AVAudioTime){
+        do {
+            if let oldCall = self.callStream {
+                var req = Cobaltspeech_Cubic_StreamingRecognizeRequest()
+                let data = Data(buffer: buffer, time: time)
+                req.audio.data = data
+                
+                try oldCall.send(req, completion: { (error) in
+                    if let er = error {
+                        self.log("Send error: \(er)")
+                    }
+                   
+                })
+               
+                self.log("sendToServer")
+            }
+       } catch let e {
+        self.log("Error \(e)")
+        do {
+            try self.callStream?.closeSend(completion: {
+                 self.log("close")
+             })
+        } catch let e {
+            self.log("Close Error \(e)")
+        }
+       }
+    }
+    func stopStream(){
+        stopAudioEngine()
+        stopGRPCStream()
+    }
+    private func stopAudioEngine(){
+        if audioEngine != nil && audioEngine.isRunning {
+            audioEngine.stop()
+            audioEngine.inputNode.removeTap(onBus: 0)
+            ExtAudioFileDispose(self.outref!)
+            try! AVAudioSession.sharedInstance().setActive(false)
+            self.isRecord = false
+           
+        }
+    }
+    private func stopGRPCStream() {
+        if let call = self.callStream {
+            do {
+                try call.closeSend {
+                    self.delegate?.streamCompletion(nil)
+                }
+            } catch let e {
+                
+            }
+        }
+    }
     @discardableResult
     public func record() -> Bool {
         //kAudioFormatMPEG4AAC default
@@ -150,4 +291,26 @@ public class CubicManager: NSObject, AVAudioRecorderDelegate {
         }
     }
     
+}
+extension Data {
+    init(buffer: AVAudioPCMBuffer, time: AVAudioTime) {
+        let audioBuffer = buffer.audioBufferList.pointee.mBuffers
+        self.init(bytes: audioBuffer.mData!, count: Int(audioBuffer.mDataByteSize))
+    }
+
+    func makePCMBuffer(format: AVAudioFormat) -> AVAudioPCMBuffer? {
+        let streamDesc = format.streamDescription.pointee
+        let frameCapacity = UInt32(count) / streamDesc.mBytesPerFrame
+        guard let buffer = AVAudioPCMBuffer(pcmFormat: format, frameCapacity: frameCapacity) else { return nil }
+
+        buffer.frameLength = buffer.frameCapacity
+        let audioBuffer = buffer.audioBufferList.pointee.mBuffers
+        
+        
+        withUnsafeBytes { addr in
+            audioBuffer.mData?.copyMemory(from: addr, byteCount: Int(audioBuffer.mDataByteSize))
+        }
+
+        return buffer
+    }
 }
