f941976cd39c2f4fee4b54841b485642749dbe2a
Eduard Miniakhmetov <eminiakhmetov@tatinfotech.com>
2020-03-13 05:11:51 +0300

    cubic-swift: Added an SDK for swift/ios.
    Adds an SDK for swift for iOS(10) and macOS(10.12) and corresponding
    documentation.

---
+++ Package.swift
@@ -0,0 +1,40 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+//
+// Copyright (2019) Cobalt Speech and Language Inc.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import PackageDescription
+
+let package = Package(
+    name: "Cubic",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "Cubic",
+            targets: ["Cubic"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/grpc/grpc-swift.git", .exact("1.0.0-alpha.9"))
+    ],
+    targets: [
+        .target(
+            name: "Cubic",
+            dependencies: ["GRPC"],
+            path: "grpc/swift-cubic")
+    ]
+)
--- grpc/swift-cubic/README.md
+++ grpc/swift-cubic/README.md
@@ -1,3 +1,9 @@
-# swift-cubic
+# Cubic
 
-A description of this package.
+## Installation
+
+See [installation docs](https://cobaltspeech.github.io/sdk-cubic/using-cubic-sdk/installation/) for details.
+
+## Usage
+
+To learn how to use Cubic SDK and see examples read our [Cubic SDK Documentation](https://cobaltspeech.github.io/sdk-cubic/using-cubic-sdk/).
