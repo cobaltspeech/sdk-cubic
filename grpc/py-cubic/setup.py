4bd1af6b48ae2db20bcf201a5f02c169105879a7
Shahruk Hossain <shahruk@cobaltspeech.com>
2020-04-25 08:15:07 +0600

    Update version to v1.5.0

--- grpc/py-cubic/setup.py
+++ grpc/py-cubic/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='cobalt-cubic',
     python_requires='>=3.5.0',
-    version='1.4.0',
+    version='1.5.0',
     description='This client library is designed to support the Cobalt API for speech recognition with Cubic',
     author='Cobalt Speech and Language Inc.',
     maintainer_email='tech@cobaltspeech.com',
