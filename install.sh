0a4515ea16151daa6fbde15af8226f43e4213d2d
Marat Tukhbatullin <>
2021-03-18 01:24:10 +0300

    save

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #tag=$(git tag --contains) && branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p' | sed 's#/#~#g') && restag=${tag:=$branch}
-for restag in $(git tag --contains) 
-do
-    echo $restag
-    if [[ "$restag" == *java ]]
-    then
-        mvn install
-    else
-        cp ./grpc/cubic.proto ./grpc/android-cubic/src/main/proto/cubic.proto
-        cd ./grpc/android-cubic && chmod +x ./gradlew && ./gradlew clean install -xtest
-    fi
-done
+echo $VERSION
+if [[ "$VERSION" == *java ]]
+then
+    echo "java"
+    mvn install
+else
+    echo "droid"
+    cp ./grpc/cubic.proto ./grpc/android-cubic/src/main/proto/cubic.proto
+    cd ./grpc/android-cubic && chmod +x ./gradlew && ./gradlew clean install -xtest
+fi
