edb175cd738684de26fced7f35f88e69fbee96b6
Marat Tukhbatullin <>
2021-03-18 01:13:42 +0300

    save

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ do
     then
         mvn install
     else
-        cp grpc/cubic.proto grpc/android-cubic/src/main/proto/cubic.proto
-        ./grpc/android-cubic && chmod +x gradlew && ./gradlew clean install -xtest
+        cp ./grpc/cubic.proto ./grpc/android-cubic/src/main/proto/cubic.proto
+        cd ./grpc/android-cubic && chmod +x ./gradlew && ./gradlew clean install -xtest
     fi
 done
