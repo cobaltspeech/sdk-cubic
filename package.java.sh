db0302e7519c2051da160934d562404d91147a0f
Julie Sheffield <julie@cobaltspeech.com>
2021-03-19 12:39:01 -0400

    doc: Apply suggestions from code review
    Co-authored-by: Jacob Holloway <jhollowayj@users.noreply.github.com>

--- package.java.sh
+++ package.java.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 if [[ "$VERSION" == *java ]]
 then
+    # Build the plain Java version
     mvn install
 else
+  # Build the Android gradle version
     cp ./grpc/cubic.proto ./grpc/android-cubic/src/main/proto/cubic.proto
     cd ./grpc/android-cubic && chmod +x ./gradlew && ./gradlew clean install -xtest
 fi
