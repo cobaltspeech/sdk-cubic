41b2b67e629f89ba2a09e0e9e54d2b7b95e0e36e
Julie Sheffield <julie@cobaltspeech.com>
2021-03-19 19:03:19 +0000

    chore: build both aar and jar in jitpack

--- package.java.sh
+++ package.java.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-if [[ "$VERSION" == *java ]]
-then
-    # Build the plain Java version
-    mvn install
-else
-  # Build the Android gradle version
-    cp ./grpc/cubic.proto ./grpc/android-cubic/src/main/proto/cubic.proto
-    cd ./grpc/android-cubic && chmod +x ./gradlew && ./gradlew clean install -xtest
-fi
+# Build the plain Java version
+mvn install
+
+# Build the Android gradle version
+cp ./grpc/cubic.proto ./grpc/android-cubic/src/main/proto/cubic.proto
+cd ./grpc/android-cubic && chmod +x ./gradlew && ./gradlew clean install -xtest && ./gradlew build publishToMavenLocal
