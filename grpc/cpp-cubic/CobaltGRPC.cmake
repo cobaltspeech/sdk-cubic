7ee90f603492872e741d42d1708113aee550f4b3
Michael Plooster <mplooster@cobaltspeech.com>
2021-07-06 12:35:32 -0600

    fix: Update the CMake gRPC build and download configuration.
    There were some errors with the configuration that were apparent
    when building on Windows. This should solve those errors (and make
    the configuration more consistent with other engines, e.g., Diatheke).

--- grpc/cpp-cubic/CobaltGRPC.cmake
+++ grpc/cpp-cubic/CobaltGRPC.cmake
@@ -5,18 +5,11 @@
 # to help us run protoc and generate c++ file from the .proto files.
 
 
-# This allows cross-compiling to work - a native version of protoc is required.
-set(GRPC_NATIVE_DIR "" CACHE PATH "Path to native gRPC installation for cross compiling.")
-
-# Check if we have already added gRPC to the project. If not, we will
-# add it here.
-message(STATUS "Checking for existing gRPC installation")
-find_package(gRPC QUIET)
-
-if(gRPC_FOUND)
-    # If we found it, try to also find the protobuf package
-    # (should be there since gRPC uses protobuf).
-    find_package(protobuf)
+# Give developers the option to use an installed version of gRPC.
+option(USE_SYSTEM_GRPC "Use system installed gRPC" OFF)
+if(USE_SYSTEM_GRPC)
+    find_package(gRPC CONFIG REQUIRED)
+    find_package(protobuf REQUIRED)
 
     # Create aliases for the imported libraries
     add_library(grpc ALIAS gRPC::grpc)
@@ -24,120 +17,114 @@ if(gRPC_FOUND)
     add_executable(protoc ALIAS protobuf::protoc)
     add_executable(grpc_cpp_plugin ALIAS grpc::grpc_cpp_plugin)
 else()
-    # Download grpc to the build directory.
+    # Download gRPC from github and add it as part of the project
     include(FetchContent)
     FetchContent_Declare(
-    gRPC
-    GIT_REPOSITORY https://github.com/grpc/grpc
-    GIT_TAG        v1.36.2
+        gRPC
+        GIT_REPOSITORY https://github.com/grpc/grpc
+        GIT_TAG        v1.36.2
     )
     set(FETCHCONTENT_QUIET OFF)
 
-    # Disable these tests for one of gRPC's third party libraries
-    # (won't build with c++11).
-    set(BENCHMARK_ENABLE_TESTING OFF CACHE BOOL "")
+    # Disable building all but the c++ plugin to speed up the build time.
+    # We don't need to build plugins for all the other languages gRPC supports.
+    set(gRPC_BUILD_GRPC_CSHARP_PLUGIN OFF CACHE INTERNAL "")
+    set(gRPC_BUILD_CSHARP_EXT OFF CACHE INTERNAL "")
+    set(gRPC_BUILD_GRPC_NODE_PLUGIN OFF CACHE INTERNAL "")
+    set(gRPC_BUILD_GRPC_OBJECTIVE_C_PLUGIN OFF CACHE INTERNAL "")
+    set(gRPC_BUILD_GRPC_PHP_PLUGIN OFF CACHE INTERNAL "")
+    set(gRPC_BUILD_GRPC_PYTHON_PLUGIN OFF CACHE INTERNAL "")
+    set(gRPC_BUILD_GRPC_RUBY_PLUGIN OFF CACHE INTERNAL "")
 
-    # Turn off the warning-as-error flag for some warnings. For some
-    # compilers, grpc won't build otherwise.
-    if(NOT WIN32)
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-error=shadow -Wno-error=attributes")
-    endif()
-
-    # If we are cross-compiling, add the native grpc dir to the CMAKE_PREFIX_PATH
-    # so that the grpc build can find it using find_program
-    if(EXISTS ${GRPC_NATIVE_DIR})
-        set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} ${GRPC_NATIVE_DIR})
-    endif()
-
-    # Make gRPC available to the project.
+    # Add gRPC to the project
     FetchContent_MakeAvailable(gRPC)
 endif()
 
-# Check if we have already defined the run_protoc function
-if(NOT COMMAND run_protoc)
-    # Define a function to conveniently run protoc. Note that the order
-    # of PROTO files is important when running the function.
-    function(run_protoc)
-        set(options "")
-        set(oneValueArgs OUTPUT_DIR OUTPUT_SUBDIR RESULT_FILES)
-        set(multiValueArgs PROTOS)
-        cmake_parse_arguments(RUN_PROTOC "${options}" "${oneValueArgs}"
-            "${multiValueArgs}" ${ARGN})
+# This allows cross-compiling to work - a native version of protoc is required.
+set(GRPC_NATIVE_DIR "" CACHE PATH "Path to native gRPC installation for cross compiling.")
 
-        # Make sure the output directory exists
-        file(MAKE_DIRECTORY ${RUN_PROTOC_OUTPUT_DIR})
+# Define a function to conveniently run protoc. Note that the order
+# of PROTO files is important when running the function.
+function(run_protoc)
+    set(options "")
+    set(oneValueArgs OUTPUT_DIR OUTPUT_SUBDIR RESULT_FILES)
+    set(multiValueArgs PROTOS)
+    cmake_parse_arguments(RUN_PROTOC "${options}" "${oneValueArgs}"
+        "${multiValueArgs}" ${ARGN})
 
-        # Formulate the actual directory where the generated files exist
-        # (depending on the proto file, sometimes protoc will output to
-        # a subdirectory within the specified output directory).
-        set(OUTDIR "${RUN_PROTOC_OUTPUT_DIR}")
-        if(RUN_PROTOC_OUTPUT_SUBDIR)
-            set(OUTDIR "${OUTDIR}/${RUN_PROTOC_OUTPUT_SUBDIR}")
-        endif()
+    # Make sure the output directory exists
+    file(MAKE_DIRECTORY ${RUN_PROTOC_OUTPUT_DIR})
 
-        # Setup the generated filenames for each proto file
-        # Get the include path for each proto file
-        if(protobuf_FOUND)
-            set(PROTOC_INCLUDE_LIST ${PROTOBUF_INCLUDE_DIR})
-        else()
-            get_filename_component(CUBIC_PROTO_DIR "${cubic_client_SOURCE_DIR}/.." ABSOLUTE)
-            set(PROTOC_INCLUDE_LIST
-                "${grpc_SOURCE_DIR}/third_party/protobuf/src"
-                "${grpc_SOURCE_DIR}/third_party/googleapis"
-                "${CUBIC_PROTO_DIR}")
-        endif()
+    # Formulate the actual directory where the generated files exist
+    # (depending on the proto file, sometimes protoc will output to
+    # a subdirectory within the specified output directory).
+    set(OUTDIR "${RUN_PROTOC_OUTPUT_DIR}")
+    if(RUN_PROTOC_OUTPUT_SUBDIR)
+        set(OUTDIR "${OUTDIR}/${RUN_PROTOC_OUTPUT_SUBDIR}")
+    endif()
 
-        foreach(protofile ${RUN_PROTOC_PROTOS})
-            get_filename_component(absolute_proto "${protofile}" ABSOLUTE)
-            get_filename_component(base_name "${absolute_proto}" NAME_WE)
-            get_filename_component(proto_path "${absolute_proto}" PATH)
+    # Set up the generated filenames for each proto file
+    # Get the include path for each proto file
+    if(protobuf_FOUND)
+        set(PROTOC_INCLUDE_LIST "${PROTOBUF_INCLUDE_DIR}")
+    else()
+        set(PROTOC_INCLUDE_LIST "${grpc_SOURCE_DIR}/third_party/protobuf/src")
+    endif()
 
-            list(APPEND PROTOC_INCLUDE_LIST "${proto_path}")
-            list(APPEND ALL_PROTOS "${absolute_proto}")
+    get_filename_component(DIATHEKE_PROTO_DIR "${diatheke_client_SOURCE_DIR}/.." ABSOLUTE)
+    list(APPEND PROTOC_INCLUDE_LIST "${DIATHEKE_PROTO_DIR}")
 
-            list(APPEND ${RUN_PROTOC_RESULT_FILES}
-                 "${OUTDIR}/${base_name}.grpc.pb.cc"
-                 "${OUTDIR}/${base_name}.grpc.pb.h"
-                 "${OUTDIR}/${base_name}.pb.cc"
-                 "${OUTDIR}/${base_name}.pb.h")
-        endforeach()
+    foreach(protofile ${RUN_PROTOC_PROTOS})
+        get_filename_component(absolute_proto "${protofile}" ABSOLUTE)
+        get_filename_component(base_name "${absolute_proto}" NAME_WE)
+        get_filename_component(proto_path "${absolute_proto}" PATH)
 
-        # Uniquify the include path list and format it properly
-        list(REMOVE_DUPLICATES PROTOC_INCLUDE_LIST)
-        if(WIN32)
-            # For windows, instead of providing a single delimited list,
-            # we will just add the include flag for each path.
-            string(REPLACE ";" " -I " PROTOC_INCLUDES "${PROTOC_INCLUDE_LIST}")
-            separate_arguments(PROTOC_INCLUDES WINDOWS_COMMAND "${PROTOC_INCLUDES}")
-        else()
-            string(REPLACE ";" ":" PROTOC_INCLUDES "${PROTOC_INCLUDE_LIST}")
-        endif()
+        list(APPEND PROTOC_INCLUDE_LIST "${proto_path}")
+        list(APPEND ALL_PROTOS "${absolute_proto}")
 
-        # To enable cross-compiling, we allow users to specify a native
-        # grpc installation, which we can use to run protoc. The version
-        # of this native grpc should be the same as the one used for
-        # cross compiling.
-        set(_PROTOC_CMD $<TARGET_FILE:protoc>)
-        set(_GRPC_CPP_PLUGIN $<TARGET_FILE:grpc_cpp_plugin>)
-        if(EXISTS ${GRPC_NATIVE_DIR})
-            get_filename_component(absolute_grpc_native "${GRPC_NATIVE_DIR}" ABSOLUTE)
-            set(_PROTOC_CMD "${absolute_grpc_native}/bin/protoc")
-            set(_GRPC_CPP_PLUGIN "${absolute_grpc_native}/bin/grpc_cpp_plugin")
-        endif()
+        list(APPEND ${RUN_PROTOC_RESULT_FILES}
+                "${OUTDIR}/${base_name}.grpc.pb.cc"
+                "${OUTDIR}/${base_name}.grpc.pb.h"
+                "${OUTDIR}/${base_name}.pb.cc"
+                "${OUTDIR}/${base_name}.pb.h")
+    endforeach()
 
-        add_custom_command(
-            OUTPUT ${${RUN_PROTOC_RESULT_FILES}}
-            COMMAND ${_PROTOC_CMD}
-            ARGS -I ${PROTOC_INCLUDES}
-              --grpc_out=${RUN_PROTOC_OUTPUT_DIR}
-              --cpp_out=${RUN_PROTOC_OUTPUT_DIR}
-              --plugin=protoc-gen-grpc=${_GRPC_CPP_PLUGIN}
-              ${ALL_PROTOS}
-            DEPENDS ${ALL_PROTOS})
+    # Uniquify the include path list and format it properly
+    list(REMOVE_DUPLICATES PROTOC_INCLUDE_LIST)
+    if(WIN32)
+        # For windows, instead of providing a single delimited list,
+        # we will just add the include flag for each path.
+        string(REPLACE ";" " -I " PROTOC_INCLUDES "${PROTOC_INCLUDE_LIST}")
+        separate_arguments(PROTOC_INCLUDES WINDOWS_COMMAND "${PROTOC_INCLUDES}")
+    else()
+        string(REPLACE ";" ":" PROTOC_INCLUDES "${PROTOC_INCLUDE_LIST}")
+    endif()
 
-        # Make sure the output variable specified makes it back to the
-        # parent scope.
-        set(${RUN_PROTOC_RESULT_FILES} ${${RUN_PROTOC_RESULT_FILES}} PARENT_SCOPE)
+    # To enable cross-compiling, we allow users to specify a native
+    # grpc installation, which we can use to run protoc. The version
+    # of this native grpc should be the same as the one used for
+    # cross compiling.
+    set(_PROTOC_CMD $<TARGET_FILE:protoc>)
+    set(_GRPC_CPP_PLUGIN $<TARGET_FILE:grpc_cpp_plugin>)
+    if(EXISTS ${GRPC_NATIVE_DIR})
+        get_filename_component(absolute_grpc_native "${GRPC_NATIVE_DIR}" ABSOLUTE)
+        set(_PROTOC_CMD "${absolute_grpc_native}/bin/protoc")
+        set(_GRPC_CPP_PLUGIN "${absolute_grpc_native}/bin/grpc_cpp_plugin")
+    endif()
 
-    endfunction(run_protoc)
-endif()
+    add_custom_command(
+        OUTPUT ${${RUN_PROTOC_RESULT_FILES}}
+        COMMAND ${_PROTOC_CMD}
+        ARGS -I ${PROTOC_INCLUDES}
+            --grpc_out=${RUN_PROTOC_OUTPUT_DIR}
+            --cpp_out=${RUN_PROTOC_OUTPUT_DIR}
+            --plugin=protoc-gen-grpc=${_GRPC_CPP_PLUGIN}
+            ${ALL_PROTOS}
+        DEPENDS ${ALL_PROTOS})
+
+    # Make sure the output variable specified makes it back to the
+    # parent scope.
+    set(${RUN_PROTOC_RESULT_FILES} ${${RUN_PROTOC_RESULT_FILES}} PARENT_SCOPE)
+
+endfunction(run_protoc)
+
