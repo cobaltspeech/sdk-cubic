be39fab64add4c0101a35a4d8f8769d3cb12aca3
shahruk10 <shahruk10@gmail.com>
2023-02-07 21:23:18 +0000

    py: update grpc deps
      - Updated googleapis-common-protos to v1.56.4, grpcio-tools to v1.48.2 which
        is the latest version that supports protobuf v3.20.0; using a later version
        will require regenerating code from the proto file with a newer protoc
        compiler which will affect multiple languages. Currently sticking with v3.20.0
        for now.
    
      - Bumped SDK version to v1.6.5.
    
      - Fixed ci issue where Jenkins needed the git directory to be marked safe.

--- grpc/py-cubic/setup.py
+++ grpc/py-cubic/setup.py
@@ -4,14 +4,15 @@ from setuptools import setup
 setup(
     name='cobalt-cubic',
     python_requires='>=3.5.0',
-    version='1.6.4',
+    version='1.6.5',
     description='This client library is designed to support the Cobalt API for speech recognition with Cubic',
     author='Cobalt Speech and Language Inc.',
     maintainer_email='tech@cobaltspeech.com',
     url='https://cobaltspeech.github.io/sdk-cubic',
     packages=["cubic"],
     install_requires=[
-        'googleapis-common-protos==1.52.0',
-        'grpcio-tools==1.35.0'
+        'googleapis-common-protos==1.56.4',
+        'grpcio-tools==1.48.2',
+        'protobuf==3.20.0'
     ]
 )
--- grpc/swift-cubic/Cubic.swift
+++ grpc/swift-cubic/Cubic.swift
@@ -12,4 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-public let CubicVersion = "1.6.4"
+public let CubicVersion = "1.6.5"
